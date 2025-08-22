87b01b5e860753aa85caa78dc79b47b9f582ed05
42kangaroo <yael.arn@bluewin.ch>
2025-08-22 15:08:15 +0200

    update optimizer class with layer parameter

--- optimizer_values.py
+++ optimizer_values.py
@@ -1,11 +1,13 @@
 import jax.numpy as jnp
 from image_converter_utils import jxl_xyb_to_srgb, srgb_to_jxl_xyb, dct_to_xyb, xyb_to_dct, upscale
 import equinox as eqx
+import jax
 
 class OptimizerValues(eqx.Module):
+    values: list[jax.Array]
 
-    def __init__(self, shape):
-        self.values = jnp.zeros(shape)
+    def __init__(self, shape, layers):
+        self.values = [jnp.zeros((shape[0]//(2**i), shape[1]//(2**i), shape[2])) for i in range(layers)]
 
     def convert_to_rgb(self):
         pass
@@ -43,9 +45,9 @@ class XYBOptimizerValues(OptimizerValues):
 
 class XYBDCTOptimizerValues(OptimizerValues):
 
-    def __init__(self, shape):
-        super().__init__(shape)
-        self.values = jnp.zeros((shape[0]//8, shape[1]//8, shape[3], 8, 8))
+    def __init__(self, shape, layers):
+        super().__init__(shape, layers)
+        self.values =[jnp.zeros((shape[0]//(2**i)//8, shape[1]//(2**i)//8, shape[2], 8, 8)) for i in range(layers)]
 
     def convert_to_xyb(self):
         return [dct_to_xyb(val) for val in self.values]
